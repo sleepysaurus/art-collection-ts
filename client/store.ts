2c15f1cb358d8c01fc86538ac522635745c6514d
sleepysaurus <ylegaspi@gmail.com>
2023-05-01 16:31:29 +1200

    add vite, remove webpack

--- client/store.ts
+++ client/store.ts
@@ -9,14 +9,19 @@ import reducers from './reducers'
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 export type ThunkAction<T = void> = BaseThunkAction<
-  T,
+  Promise<T>,
   RootState,
   void,
   AnyAction
 >
 
-const store = createStore(
-  reducers,
-  composeWithDevTools(applyMiddleware(thunkMiddleware)) // NEW
-)
+export function initialiseStore() {
+  return createStore(
+    reducers,
+    composeWithDevTools(applyMiddleware(thunkMiddleware))
+  )
+}
+
+const store = initialiseStore()
+
 export default store
